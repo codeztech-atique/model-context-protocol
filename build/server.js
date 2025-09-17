43bd18212d0d5773aebd3ab2906e7bedc25ac6b7
Atique Ahmed <atique1224@gmail.com>
2025-09-17 09:49:35 +0530

    Fix mcp server issue

--- build/server.js
+++ build/server.js
@@ -5,62 +5,152 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mcp_js_1 = require("@modelcontextprotocol/sdk/server/mcp.js");
 const stdio_js_1 = require("@modelcontextprotocol/sdk/server/stdio.js");
+const zod_1 = require("zod");
 const promises_1 = __importDefault(require("node:fs/promises"));
-const zod_1 = __importDefault(require("zod"));
+const types_js_1 = require("@modelcontextprotocol/sdk/types.js");
 const server = new mcp_js_1.McpServer({
-    name: "test",
-    version: "1:0:0",
-    capabilites: {
-        resouces: {},
+    name: "test-video",
+    version: "1.0.0",
+    capabilities: {
+        resources: {},
         tools: {},
-        prompts: {}
-    }
+        prompts: {},
+    },
 });
 server.resource("users", "users://all", {
     description: "Get all users data from the database",
     title: "Users",
-    mimeType: "application/json"
+    mimeType: "application/json",
 }, async (uri) => {
     const users = await import("./data/users.json", {
-        with: { type: "json" }
+        with: { type: "json" },
     }).then(m => m.default);
     return {
         contents: [
-            { uri: uri.href, text: JSON.stringify(users), mimeType: "application/json" }
-        ]
+            {
+                uri: uri.href,
+                text: JSON.stringify(users),
+                mimeType: "application/json",
+            },
+        ],
+    };
+});
+server.resource("user-details", new mcp_js_1.ResourceTemplate("users://{userId}/profile", { list: undefined }), {
+    description: "Get a user's details from teh database",
+    title: "User Details",
+    mimeType: "application/json",
+}, async (uri, { userId }) => {
+    const users = await import("./data/users.json", {
+        with: { type: "json" },
+    }).then(m => m.default);
+    const user = users.find(u => u.id === parseInt(userId));
+    if (user == null) {
+        return {
+            contents: [
+                {
+                    uri: uri.href,
+                    text: JSON.stringify({ error: "User not found" }),
+                    mimeType: "application/json",
+                },
+            ],
+        };
+    }
+    return {
+        contents: [
+            {
+                uri: uri.href,
+                text: JSON.stringify(user),
+                mimeType: "application/json",
+            },
+        ],
     };
 });
 server.tool("create-user", "Create a new user in the database", {
-    name: zod_1.default.string(),
-    email: zod_1.default.string(),
-    address: zod_1.default.string(),
-    phone: zod_1.default.string()
+    name: zod_1.z.string(),
+    email: zod_1.z.string(),
+    address: zod_1.z.string(),
+    phone: zod_1.z.string(),
 }, {
     title: "Create User",
     readOnlyHint: false,
     destructiveHint: false,
     idempotentHint: false,
-    openWorldHint: true
+    openWorldHint: true,
 }, async (params) => {
     try {
         const id = await createUser(params);
         return {
-            content: [
-                { type: "text", text: `User ${id} created successfully ` }
-            ]
+            content: [{ type: "text", text: `User ${id} created successfully` }],
         };
     }
-    catch (error) {
+    catch {
         return {
-            content: [
-                { type: "text", text: "Failed to saved user" }
-            ]
+            content: [{ type: "text", text: "Failed to save user" }],
         };
     }
 });
+server.tool("create-random-user", "Create a random user with fake data", {
+    title: "Create Random User",
+    readOnlyHint: false,
+    destructiveHint: false,
+    idempotentHint: false,
+    openWorldHint: true,
+}, async () => {
+    const res = await server.server.request({
+        method: "sampling/createMessage",
+        params: {
+            messages: [
+                {
+                    role: "user",
+                    content: {
+                        type: "text",
+                        text: "Generate fake user data. The user should have a realistic name, email, address, and phone number. Return this data as a JSON object with no other text or formatter so it can be used with JSON.parse.",
+                    },
+                },
+            ],
+            maxTokens: 1024,
+        },
+    }, types_js_1.CreateMessageResultSchema);
+    if (res.content.type !== "text") {
+        return {
+            content: [{ type: "text", text: "Failed to generate user data" }],
+        };
+    }
+    try {
+        const fakeUser = JSON.parse(res.content.text
+            .trim()
+            .replace(/^```json/, "")
+            .replace(/```$/, "")
+            .trim());
+        const id = await createUser(fakeUser);
+        return {
+            content: [{ type: "text", text: `User ${id} created successfully` }],
+        };
+    }
+    catch {
+        return {
+            content: [{ type: "text", text: "Failed to generate user data" }],
+        };
+    }
+});
+server.prompt("generate-fake-user", "Generate a fake user based on a given name", {
+    name: zod_1.z.string(),
+}, ({ name }) => {
+    return {
+        messages: [
+            {
+                role: "user",
+                content: {
+                    type: "text",
+                    text: `Generate a fake user with the name ${name}. The user should have a realistic email, address, and phone number.`,
+                },
+            },
+        ],
+    };
+});
 async function createUser(user) {
     const users = await import("./data/users.json", {
-        with: { type: "json" }
+        with: { type: "json" },
     }).then(m => m.default);
     const id = users.length + 1;
     users.push({ id, ...user });
